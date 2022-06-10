8cd4cc9e8734e72af86e07302dfbe79ee984e210
Catherine Tung <catt.tung@gmail.com>
2022-06-10 12:12:03 -0400

    Comment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Codewars 6kyu Replace with Alphabet Position
 //In this kata you are required to, given a string, replace every letter with its position in the alphabet. If anything in the text isn't a letter, ignore it and don't return it. "a" = 1, "b" = 2, etc.
+//Uses RegEx to remove special characters - resource https://bobbyhadz.com/blog/javascript-remove-special-characters-from-string
 
 function alphabetPosition(text) {
   //first lowercase everything
