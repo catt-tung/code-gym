48a926e976e8fe0edf058975678578d16b0029b2
Catherine Tung <catt.tung@gmail.com>
2022-06-11 15:53:31 -0400

    Array difference codewars challenge solution

--- app.js
+++ app.js
@@ -12,3 +12,42 @@ function alphabetPosition(text) {
     }
     return arrayNum.join(' ')
 }
+
+//other solutions
+function alphabetPosition2(text) {
+  return text
+    .toUpperCase()
+    .match(/[a-z]/gi)
+    .map( (c) => c.charCodeAt() - 64)
+    .join(' ');
+}
+
+//--------------------------
+
+//Codewars 6kyu Array.diff
+//Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result. It should remove all values from list a, which are present in list b keeping their order.
+
+function arrayDiff(a, b) {
+  if (a.length === 0 || b.length === 0) {
+    return a
+  } else {
+    for (let i=0; i<b.length; i++) {
+      a = a.filter(elem => elem !== b[i])
+    }
+  } return a
+}
+
+// other solutions:
+function array_diff(a, b) {
+  return a.filter(e => !b.includes(e));
+}
+function array_diff2(a, b) {
+  b = new Set(b)
+  return a.filter(v => !b.has(v))
+}
+//set??
+var array_diff = (a, b) => a.filter(item => b.indexOf(item) < 0)
+function array_diff3(a, b) {
+  return a.filter(x => b.indexOf(x) == -1);
+}
+// index of negative?
