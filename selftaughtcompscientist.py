a655184a2fb24159c1f9bdd2e8e5ce9d276998ef
Catherine Tung <catt.tung@gmail.com>
2022-06-18 14:34:07 -0400

    Anagram string algorithm

--- selftaughtcompscientist.py
+++ selftaughtcompscientist.py
@@ -43,3 +43,14 @@ def merge_sort(a_list):
             right_ind += 1
             alist_ind += 1
     return a_list
+
+#Anagram
+def is_anagram(str1, str2):
+    str1 = str1.replace(' ','').lower()
+    str2 = str2.replace(' ','').lower()
+    if sorted(str1) == sorted(str2):
+        return True
+    else:
+        return False
+
+print(is_anagram('Tom Marvolo Riddle', 'I am Lord Voldemort'))
