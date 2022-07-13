c169efe640265909e66f9f077b2ffba4015e52bd
Catherine Tung <catt.tung@gmail.com>
2022-07-12 22:03:20 -0400

    Square class list

--- selftaughtprogrammer.py
+++ selftaughtprogrammer.py
@@ -84,3 +84,28 @@ r3 = Rectangle(100,200)
 
 print(Rectangle.recs)
 #(result is [(10, 24), (20, 40), (100, 200)])
+
+class Shape():
+    def what_am_i(self):
+        print("I am a shape.")
+
+
+class Square(Shape):
+    square_list = []
+
+    def __init__(self, s1):
+        self.s1 = s1
+        self.square_list.append(self)
+
+    def calculate_perimeter(self):
+        return self.s1 * 4
+
+    def what_am_i(self):
+        super().what_am_i()
+        print("I am a Square.")
+
+
+a_square = Square(29)
+print(Square.square_list)
+another_square = Square(93)
+print(Square.square_list)
