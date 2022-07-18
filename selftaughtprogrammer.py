dfff904dca4b59f65e1f5ebbbd76ce4116487286
Catherine Tung <catt.tung@gmail.com>
2022-07-18 14:55:21 -0400

    Queue

--- selftaughtprogrammer.py
+++ selftaughtprogrammer.py
@@ -359,3 +359,34 @@ for i in range(len(string.items)):
     reverse += string.pop()
 
 print(reverse)
+
+#Queue
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+a_queue = Queue()
+
+for i in range(5):
+    a_queue.enqueue(i)
+
+print(a_queue.size())
+
+for i in range(5):
+    a_queue.dequeue()
+
+print(a_queue.is_empty())
