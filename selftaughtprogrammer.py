5b7d1e68443f84267e0df2bb5c57e9d4ac0fc94d
Catherine Tung <catt.tung@gmail.com>
2022-07-18 17:33:10 -0400

    FizzBuzz in Python

--- selftaughtprogrammer.py
+++ selftaughtprogrammer.py
@@ -475,3 +475,16 @@ for k in range(len(stack.items)):
     reversed_stack.append(stack.pop())
 
 print(reversed_stack)
+
+#FizzBuzz with Python
+
+def fizzbuzz():
+    for i in range (1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
