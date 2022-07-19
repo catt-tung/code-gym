4bdb2c04823161e25c69a4eed65ca680d1b74e00
Catherine Tung <catt.tung@gmail.com>
2022-07-19 10:45:44 -0400

    Sequential search example

--- selftaughtprogrammer.py
+++ selftaughtprogrammer.py
@@ -488,3 +488,18 @@ def fizzbuzz():
             print("Fizz")
         else:
             print(i)
+
+# sequential search
+def ss(number_list, n):
+    found = False
+    for i in number_list:
+        if i == n:
+            found = True
+            break
+    return found
+
+numbers = range(0, 100)
+s1 = ss(numbers, 2)
+print(s1)
+s2 = ss(numbers, 202)
+print(s2)
