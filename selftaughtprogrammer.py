81761d6dff103f8932acbc5c4ef74b5b270bb0bf
Catherine Tung <catt.tung@gmail.com>
2022-07-20 20:41:49 -0400

    Recursive function example with bottles of beer lyrics

--- selftaughtprogrammer.py
+++ selftaughtprogrammer.py
@@ -520,3 +520,17 @@ def is_anagram(w1, w2):
 
 print(is_anagram("iceman", "cinema"))
 print(is_anagram("now", "wow"))
+
+# Recursive function example with 99 bottles of beer
+def bottles_of_beer(bob):
+    #base case
+    if bob < 1:
+        print("""No more bottles of beer on the wall. No more bottles of beer.""")
+        return
+    tmp = bob
+    bob -= 1
+    print("""{} bottles of beer on the wall. {} bottles of beer. Take one down, pass it around, {} bottles of beeron the wall.""".format(tmp, tmp, bob))
+    #as long as base case is not satisfied, call the function:
+    bottles_of_beer(bob)
+
+bottles_of_beer(99)
