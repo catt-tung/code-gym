d72e80a38cd45f5a1b58ae57779a1e9ab62d0e1c
Catherine Tung <catt.tung@gmail.com>
2022-09-10 18:11:39 -0400

    Linked List Python

--- selftaughtcompscientist.py
+++ selftaughtcompscientist.py
@@ -228,6 +228,19 @@ return_sorted_nums(an_array)
 
 #Creating a linked list
 class Node:
-    def__init__(self, data, next=None):
+    def __init__(self, data, next=None):
         self.data = data
         self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = Node(data)
