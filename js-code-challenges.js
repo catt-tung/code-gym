ffeb5e42e6db147e919bcc805c60b73a8003c8db
Catherine Tung <catt.tung@gmail.com>
2022-09-16 22:29:16 -0400

    Adding daily code challenges that were not pushing in the other repo

---
+++ js-code-challenges.js
@@ -0,0 +1,48 @@
+// Adding these since my forked repository hasn't been pushing
+/*-----------------------------------------------------------------------------
+Challenge: 14-fromPairs
+
+Difficulty: Intermediate
+
+Prompt:
+
+- Write a function named fromPairs that creates an object from an array containing nested arrays.
+- Each nested array will have two elements representing key/value pairs used to create key/value pairs in an object to be returned by the function.
+- If a key appears in multiple pairs, the rightmost pair should overwrite the previous entry in the object.
+
+Examples:
+
+fromPairs([ ['a', 1], ['b', 2], ['c', 3] ]) //=> { a: 1, b: 2, c: 3 }
+fromPairs([ ['name', 'Sam"], ['age', 24], ['name', 'Sally'] ]) //=> { name: "Sally", age: 24 }
+-----------------------------------------------------------------------------*/
+// Your solution for 14-fromPairs here:
+// testing git updates
+function fromPairs(arr){
+  let obj = {}
+  for (let i = 0; i < arr.length; i++) {
+    obj[arr[i][0]] = arr[i][1]
+  }
+  return obj
+}
+
+/*-----------------------------------------------------------------------------
+Challenge: 15-mergeObjects
+
+Difficulty: Intermediate
+
+Prompt:
+
+- Write a function named mergeObjects that accepts at least two objects as arguments, merges the properties of the second through n objects into the first object, then finally returns the first object.
+- If any objects have the same property key, values from the object(s) later in the arguments list should overwrite earlier values.
+
+Examples:
+
+mergeObjects({}, {a: 1}); //=> {a: 1} (same object as first arg)
+mergeObjects({a: 1, b: 2, c: 3}, {d: 4}); //=> {a: 1, b: 2, c: 3, d: 4}
+mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44}); //=> {a: 1, b: 22, c: 3, d: 44}
+-----------------------------------------------------------------------------*/
+// Your solution for 15-mergeObjects here:
+function mergeObjects(target, ...source) {
+  let final = Object.assign(target, ...source)
+  return final
+}
